816c6d408a2929605b663eeb9ffb563b57298700
pi <pimail31@gmail.com>
2016-09-29 17:50:25 +0900

    첫페이지 예제 목록 안나오는 문제 수정

--- RxDobbyExample/ExamListViewController.swift
+++ RxDobbyExample/ExamListViewController.swift
@@ -64,15 +64,6 @@ class ExamListViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
 
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
